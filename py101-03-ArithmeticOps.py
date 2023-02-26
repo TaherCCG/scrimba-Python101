07401625fa4b0ee238c2674e634b841597daa2cc
TaherCCG <taher.ccg@gmail.com>
2023-02-26 21:53:10 +0000

    Python 101: Basic Arithmetic Operations

---
+++ py101-03-ArithmeticOps.py
@@ -0,0 +1,13 @@
+print ("Basic Arithmetic Operations")
+
+a=10
+b=3
+print ("a={}\nb={}\n".format(a,b))
+
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
