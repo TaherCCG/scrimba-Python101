63568c09ad2098a1121e76e539d27baf9fce3653
TaherCCG <taher.ccg@gmail.com>
2023-02-26 21:30:23 +0000

    python 101 lesson 02 datatypes and typecasting

---
+++ py101-lo2-Datatypes-Typecasting.py
@@ -0,0 +1,25 @@
+a = int(1)        # a will be 1
+b = int(2.5)      # b will be 2
+c = int("3")      # c will be 3
+c1 = int(float("3.4"))   # c1 will be float
+d = float(1)      # d will be 1.0
+e = float(2.5)    # e will be 2.5
+f = float("3")    # f will be 3.0
+g = float("4.23") # g will be 4.23
+h = str("80s")    # h will be '80s'
+i = str(22)       # i will be '22'
+j = str(3.01)     # j will be '3.01'
+
+print([a,b,c,c1,d,e,f,g,h,i,j])
+print()
+print('this is {} ==> {}'.format(type(a),a))
+print('this is {} ==> {}'.format(type(b),b))
+print('this is {} ==> {}'.format(type(c),c))
+print('this is {} ==> {}'.format(type(c1),c1))
+print('this is {} ==> {}'.format(type(d),d))
+print('this is {} ==> {}'.format(type(e),e))
+print('this is {} ==> {}'.format(type(f),f))
+print('this is {} ==> {}'.format(type(g),g))
+print('this is {} ==> {}'.format(type(h),h))
+print('this is {} ==> {}'.format(type(i),i))
+print('this is {} ==> {}'.format(type(j),j))
