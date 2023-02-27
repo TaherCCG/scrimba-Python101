3456bb8bd0894c9306eb1ad83a45a79c6c057e61
TaherCCG <taher.ccg@gmail.com>
2023-02-27 20:31:50 +0000

    changed name to organise files

---
+++ py101-ex01-VarDatatypes.py
@@ -0,0 +1,8 @@
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+
+item_name = 'Item'
+price = 20.99
+inventory=50
+print(item_name,price,inventory)
